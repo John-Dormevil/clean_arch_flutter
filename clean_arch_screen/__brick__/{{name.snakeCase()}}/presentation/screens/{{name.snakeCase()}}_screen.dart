43f9ad5dcc144e553a0baa0df6fba222e6b1a205
dormevil.jean-jules <jean-jules.dormevil@gmail.com>
2023-09-28 00:51:55 +0200

    add missing go_router import

--- clean_arch_screen/__brick__/{{name.snakeCase()}}/presentation/screens/{{name.snakeCase()}}_screen.dart
+++ clean_arch_screen/__brick__/{{name.snakeCase()}}/presentation/screens/{{name.snakeCase()}}_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 final {{name.camelCase()}}ScreenRoute = GoRoute(
   path: {{name.pascalCase()}}Screen.path,
