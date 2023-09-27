89b2592e861de57b9385e4ac4ebd537d30851744
dormevil.jean-jules <jean-jules.dormevil@gmail.com>
2023-09-27 23:48:43 +0200

    draft first brick to implement presentation layer

---
+++ clean_arch_screen/__brick__/{{name.snakeCase()}}/presentation/screens/{{name.snakeCase()}}_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final {{name.camelCase()}}ScreenRoute = GoRoute(
+  path: {{name.pascalCase()}}Screen.path,
+  builder: (context, state) => const {{name.pascalCase()}}Screen(),
+);
+
+class {{name.pascalCase()}}Screen extends ConsumerWidget {
+  static const path = '/{{name.snakeCase()}}';
+  static const name = '{{name.snakeCase()}}';
+
+  const {{name.pascalCase()}}Screen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Center(child: Text("{{name.pascalCase()}} screen"));
+  }
+}
